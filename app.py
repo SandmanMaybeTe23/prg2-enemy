93ed78bb92ef1978d4e59d5d41da3eb08f78e7eb
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-10-03 10:57:02 +0200

    while

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 
+
 class Enemy:
     def __init__(self, health:int,attack:int,name:str):
         self.hp=health
         self.atk=attack
         self.name=name
-
 
 
     def print_status(self):
@@ -13,22 +13,35 @@ class Enemy:
 
     def attacks(self):
         print(f"{self.name} has attack you with {self.atk} dmg and it hurt so much")
-
+        
+        
 
     def take_dmg(self,dmg):
         self.hp -= dmg 
         print(f"{self.name} has taken {dmg} and has {self.hp} left")
+
         
     def are_you_alive(self):
-         if self.hp<0:
-             print(f"{self.name} dead")
+         while True:
+            if self.hp >= 0:
+                 print(f"{self.name} dead")
+                 break
+            else:
+                pass
 
 
-enemy_1=Enemy(10,5,"slumbery")
+slime=Enemy(100,5,"Slumbery")
+goblin= Enemy(50,5,"Glen")
 
-enemy_1.print_status()
+slime.print_status()
 
-enemy_1.attacks()
+goblin.print_status()
 
+slime.are_you_alive()
+goblin.are_you_alive()
 
-
+goblin.attacks()
+while True:
+    slime.take_dmg(goblin.atk)
+    goblin.take_dmg(slime.atk)
+    
